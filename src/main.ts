dd847a10fa7c372f7ad450ae24f18047595d8ce1
Vance <VancePMorrison@gmail.com>
2026-03-01 08:04:42 -0800

    remove instrumentation

--- src/main.ts
+++ src/main.ts
@@ -17,10 +17,6 @@ mobileDragDropPolyfill({
 import { callerBuddy } from "./caller-buddy.js";
 import "./components/app-shell.js";
 
-// #region agent log
-fetch('http://127.0.0.1:7242/ingest/0cdf7510-7efc-4442-ba7b-a97899c3747e',{method:'POST',headers:{'Content-Type':'application/json','X-Debug-Session-Id':'e72d3c'},body:JSON.stringify({sessionId:'e72d3c',location:'main.ts:startup',message:'App main.ts executing - polyfill loaded, about to init',data:{timestamp:Date.now()},timestamp:Date.now(),hypothesisId:'H1'})}).catch(()=>{});
-// #endregion
-
 // Initialize the CallerBuddy application
 callerBuddy.init();
 
